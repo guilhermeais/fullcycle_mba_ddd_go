bf14127ebc0ffe6b25df8e67b0774da67e3ffe18
Guilherme Teixeira Ais <73388069+guilhermeais@users.noreply.github.com>
2025-03-11 17:08:40 -0300

    test: new test case for birthday

--- internal/common/birthday_test.go
+++ internal/common/birthday_test.go
@@ -17,6 +17,7 @@ func TestBirtday(t *testing.T) {
 	cases := testCases{
 		{date: time.Date(2003, 8, 26, 0, 0, 0, 0, time.UTC), now: time.Date(2025, 1, 1, 0, 0, 0, 0, time.UTC), expectedAge: 21},
 		{date: time.Date(2004, 8, 26, 0, 0, 0, 0, time.UTC), now: time.Date(2025, 1, 1, 0, 0, 0, 0, time.UTC), expectedAge: 20},
+		{date: time.Date(2004, 1, 2, 0, 0, 0, 0, time.UTC), now: time.Date(2025, 1, 3, 0, 0, 0, 0, time.UTC), expectedAge: 21},
 	}
 
 	for _, testCase := range cases {
